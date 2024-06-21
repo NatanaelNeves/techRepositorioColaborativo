13dc2ed4ba01ab6d5112722d657dc32533319618
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-06-21 16:56:02 -0300

    atualização javascritp

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome =  'natan' 
+
+let numero 
+
+// alert('olá ' +  nome)
+
+console.log(numero)
+
+// entrada de texto
+let entradaUsuario = prompt('digite seu CPF')
+
+numero = confirm('deseja fazer seu cartao C&A?')
