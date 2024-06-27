9734b3adaf18651c6f974af3814c3c67b369dc01
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-06-27 16:59:46 -0300

    exercicio js

--- exerciciosjs/contaBancariaValidacao.js
+++ exerciciosjs/contaBancariaValidacao.js
@@ -1,14 +1,27 @@
 let senha = 123
-let tentativas = 0;
+let tentativas = 3
+let contTentativas = 1
 let senhaUser = prompt('Digite a sua senha');
 
 
 if (senha == senhaUser) {
-    alert('Caminho Feliz :D');
+    alert('Acessou a conta!');
 } else {
-    while (senha != senhaUser && tentativas < 3) {
-        senhaUser = prompt('Senha incorreta. Digite novamente:');
-        tentativas++;
+    while (senha != senhaUser && contTentativas < 3) {
+        senhaUser = prompt(`Senha incorreta. Você tem ${tentativas-contTentativas} tentativas. Digite novamente a senha.`);
+
+
+        if(senha == senhaUser){
+            alert('Entrou na conta')
+
+
+        }
+        else{
+            contTentativas+=1
+
+        }
+        
     }
     alert('Limite de tentativas excedido. Acesso bloqueado.');
+
 }
